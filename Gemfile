4d8067aa40b09c8fe13cf8743a24ca79ca272cdb
jacobn9000wow <jacob_n90wow@hotmail.com>
2012-12-10 22:38:15 -0800

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -5,8 +5,13 @@ gem 'rails', '3.2.7'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
+gem 'bing'
+
 gem 'bootstrap-sass', '2.0.4'
 gem 'bcrypt-ruby', '3.0.1'
+
+gem 'will_paginate', '3.0.3'
+gem 'bootstrap-will_paginate', '0.0.6'
 
 group :development do
   gem 'sqlite3'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy] #The authorization application code uses a before filter, which arranges for a particular method to be called before the given actions. To require users to be signed in, we define a signed_in_user method and invoke it using before_filter :signed_in_user, 
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
   def new
     @user = User.new
   end
@@ -21,5 +36,39 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        #redirect_to signin_url, notice: "Please sign in."
+ 	flash[:notice] = "Please sign in."
+	redirect_to signin_path
+      end
+
+#uses a shortcut for setting flash[:notice] by passing an options hash to the redirect_to function. Together with :success and :error, the :notice key completes our triumvirate of flash styles, all of which are supported natively by Bootstrap CSS. http://ruby.railstutorial.org/chapters/updating-showing-and-deleting-users#code-authorize_before_filter
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      flash[:notice] = "Please sign in."
+      redirect_to(root_path) unless current_user?(@user)  #current_user? defined in sessions helper
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 
 end
